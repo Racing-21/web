1da2f77f8921500a86c89ef6c8cb647d68896137
Jan Dudek <honza@honzadudek.cz>
2024-09-13 17:49:57 +0200

    feat(homepage): remove unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "../styles/globals.css";
 import Navigation from "@/app/ui/navigation";
 import Footer from "@/app/ui/Footer";
-import Script from "next/script";
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
