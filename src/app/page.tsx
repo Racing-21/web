e7b11170146532753ae4fc36cedb224dedff4730
Jan Dudek <honza@honzadudek.cz>
2024-09-13 10:23:22 +0200

    feat(navigation): setup basic navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import Head from "next/head";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: 'Racing21',
+}
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
-	    <Head>
-		    <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
-
-	    </Head>
 	    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
--- src/app/ui/navigation.tsx
+++ src/app/ui/navigation.tsx
@@ -1,96 +1,196 @@
+"use client";
+
+import { useState } from "react";
+import logo from "@/images/logo.png";
+import Image from "next/image";
+import {
+	Dialog,
+	DialogPanel,
+	Disclosure,
+	DisclosureButton,
+	DisclosurePanel,
+	Popover,
+	PopoverButton,
+	PopoverGroup,
+	PopoverPanel,
+} from "@headlessui/react";
+import {
+	Bars3Icon,
+	ChartPieIcon,
+	CursorArrowRaysIcon,
+	FingerPrintIcon,
+	SquaresPlusIcon,
+	XMarkIcon,
+} from "@heroicons/react/24/outline";
+import {
+	ChevronDownIcon,
+	PhoneIcon,
+	PlayCircleIcon,
+	RectangleGroupIcon,
+} from "@heroicons/react/20/solid";
+
+const about = [
+	{
+		name: "Náš tým",
+		description: "Informace o členech závodního týmu Racing 21",
+		href: "/o-nas/tym",
+		icon: ChartPieIcon,
+	},
+	{
+		name: "Technika",
+		description: "Specifikace našich závodních speciálů a vybavení",
+		href: "/o-nas/tym",
+		icon: CursorArrowRaysIcon,
+	},
+	{
+		name: "Výsledky",
+		description: "Přehled úspěchů týmu Racing 21 v závodech v české republice i ve světě",
+		href: "/o-nas/tym",
+		icon: FingerPrintIcon,
+	},
+];
 
 export default function Navigation() {
-	return <div className="bg-gray-800">
-		<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-			<div className="relative flex h-16 items-center justify-between">
-				<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-					<button type="button"
-					        className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-					        aria-controls="mobile-menu" aria-expanded="false">
-						<span className="absolute -inset-0.5"></span>
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+	return (
+		<header className="relative isolate z-10 bg-grayPrimary">
+			<nav
+				aria-label="Global"
+				className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+			>
+				<div className="flex lg:flex-1">
+					<a href="#" className="-m-1.5 p-1.5">
+						<span className="sr-only">Racing21</span>
+						<Image alt="" src={logo} className="h-8 w-auto" />
+					</a>
+				</div>
+				<div className="flex lg:hidden">
+					<button
+						type="button"
+						onClick={() => setMobileMenuOpen(true)}
+						className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+					>
 						<span className="sr-only">Open main menu</span>
-						<svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						     stroke="currentColor"
-						     aria-hidden="true">
-							<path stroke-linecap="round" stroke-linejoin="round"
-							      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-						</svg>
-						<svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						     stroke="currentColor"
-						     aria-hidden="true">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
-						</svg>
+						<Bars3Icon aria-hidden="true" className="h-6 w-6" />
 					</button>
 				</div>
-				<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-					<div className="flex flex-shrink-0 items-center">
-						<img className="h-8 w-auto"
-						     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-						     alt="Your Company"/>
+				<PopoverGroup className="hidden lg:flex lg:gap-x-12 bg-grayPrimary">
+					<Popover>
+						<PopoverButton className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-50">
+							Náš tým
+							<ChevronDownIcon
+								aria-hidden="true"
+								className="h-5 w-5 flex-none text-gray-200"
+							/>
+						</PopoverButton>
+
+						<PopoverPanel
+							transition
+							className="absolute inset-x-0 top-0 -z-10 bg-grayPrimary pt-14 shadow-lg ring-1 ring-gray-900/5 transition data-[closed]:-translate-y-1 data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-150 data-[enter]:ease-out data-[leave]:ease-in"
+						>
+							<div className="mx-auto grid max-w-7xl grid-cols-4 gap-x-4 px-6 py-10 lg:px-8 xl:gap-x-8">
+								{about.map((item) => (
+									<div
+										key={item.name}
+										className="group relative rounded-lg p-6 text-sm leading-6 hover:bg-gray-50"
+									>
+										<div className="flex h-11 w-11 items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+											<item.icon
+												aria-hidden="true"
+												className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+											/>
+										</div>
+										<a
+											href={item.href}
+											className="mt-6 block font-semibold text-gray-900"
+										>
+											{item.name}
+											<span className="absolute inset-0" />
+										</a>
+										<p className="mt-1 text-gray-600">{item.description}</p>
+									</div>
+								))}
+							</div>
+						</PopoverPanel>
+					</Popover>
+
+					<a href="#" className="text-sm font-semibold leading-6 text-gray-50">
+						Autodílna
+					</a>
+					<a href="#" className="text-sm font-semibold leading-6 text-gray-50">
+						Shop
+					</a>
+					<a href="#" className="text-sm font-semibold leading-6 text-gray-50">
+						Pronájem
+					</a>
+				</PopoverGroup>
+				<div className="hidden lg:flex lg:flex-1 lg:justify-end"></div>
+			</nav>
+			<Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+				<div className="fixed inset-0 z-10" />
+				<DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-grayPrimary sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+					<div className="flex items-center justify-between px-6 py-6">
+						<a href="#" className="-m-1.5 p-1.5">
+							<span className="sr-only">Your Company</span>
+							<Image alt="" src={logo} className="h-8 w-auto" />
+						</a>
+						<button
+							type="button"
+							onClick={() => setMobileMenuOpen(false)}
+							className="-m-2.5 rounded-md p-2.5 text-gray-100"
+						>
+							<span className="sr-only">Close menu</span>
+							<XMarkIcon aria-hidden="true" className="h-6 w-6" />
+						</button>
 					</div>
-					<div className="hidden sm:ml-6 sm:block">
-						<div className="flex space-x-4">
-							<a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-							   aria-current="page">Dashboard</a>
-							<a href="#"
-							   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-							<a href="#"
-							   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-							<a href="#"
-							   className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
+					<div className="flow-root bg-white px-6 grow-1 h-full">
+						<div className="divide-y divide-gray-500/10">
+							<div className="space-y-2 py-6">
+								<Disclosure as="div" className="-mx-3">
+									<DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+										O nás
+										<ChevronDownIcon
+											aria-hidden="true"
+											className="h-5 w-5 flex-none group-data-[open]:rotate-180"
+										/>
+									</DisclosureButton>
+									<DisclosurePanel className="mt-2 space-y-2">
+										{[...about].map((item) => (
+											<DisclosureButton
+												key={item.name}
+												as="a"
+												href={item.href}
+												className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+											>
+												{item.name}
+											</DisclosureButton>
+										))}
+									</DisclosurePanel>
+								</Disclosure>
+								<a
+									href="#"
+									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+								>
+									Autodílna
+								</a>
+								<a
+									href="#"
+									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+								>
+									Shop
+								</a>
+								<a
+									href="#"
+									className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+								>
+									Pronájem
+								</a>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div
-					className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-					<button type="button"
-					        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-						<span className="absolute -inset-1.5"></span>
-						<span className="sr-only">View notifications</span>
-						<svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-						     stroke="currentColor"
-						     aria-hidden="true">
-							<path stroke-linecap="round" stroke-linejoin="round"
-							      d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"/>
-						</svg>
-					</button>
-					<div className="relative ml-3">
-						<div>
-							<button type="button"
-							        className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-							        id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-								<span className="absolute -inset-1.5"></span>
-								<span className="sr-only">Open user menu</span>
-								<img className="h-8 w-8 rounded-full"
-								     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-								     alt=""/>
-							</button>
-						</div>
-						<div
-							className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-							role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
-							<a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1"
-							   id="user-menu-item-0">Your Profile</a>
-							<a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1"
-							   id="user-menu-item-1">Settings</a>
-							<a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1"
-							   id="user-menu-item-2">Sign out</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div className="sm:hidden" id="mobile-menu">
-			<div className="space-y-1 px-2 pb-3 pt-2">
-				<a href="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-				   aria-current="page">Dashboard</a>
-				<a href="#"
-				   className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-				<a href="#"
-				   className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-				<a href="#"
-				   className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
-			</div>
-		</div>
-	</div>
+				</DialogPanel>
+			</Dialog>
+		</header>
+	);
 }
