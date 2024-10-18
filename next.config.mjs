565ebec1ba6dc5d46f15831d733799e432c95f9f
Jan Dudek <honza@honzadudek.cz>
2024-10-18 11:07:01 +0200

    feat(CMS): update image setting for Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [{
-			protocol: 'http',
-			hostname: 'www.racing21.cz',
-		}],
-	}
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "www.racing21.cz",
+			},
+		],
+		domains: ["assets.tina.io"],
+	},
 };
 
 export default nextConfig;
