15ecc92e63e8d49bb4f62de177ad84f610911998
Jan Dudek <honza@honzadudek.cz>
2024-10-25 17:56:57 +0200

    feat(venues): add venuses content

--- pronajem/Prostory.md
+++ pronajem/Prostory.md
@@ -4,11 +4,49 @@ prostory:
     shortDescription: Rozsáhlý prostor pro větší akce
     longDescription: >
       Náš největší prostor, který pojme až 50 účastníků. K dispozici je stylový
+      bar a komplet audioviuální vybavení. Vybavení:
+
+
+      * Rychlé wi-fi připojení
+
+      * Možnost cateringu
+
+      * Možnost přizpůsobení prostoru Vašim potřebám
+    image: >-
+      https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/09_gzauxj.webp
+    gallery:
+      - >-
+        https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/08_ebvm4p.webp
+      - >-
+        https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/010_cfsk0k.webp
+      - >-
+        https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/07_tqrxec.webp
+  - name: Showroom
+    shortDescription: Rozsáhlý prostor pro větší akce
+    longDescription: >
+      Náš největší prostor, který pojme až 50 účastníků. K dispozici je stylový
       bar a komplet audioviuální vybavení.
-    image:
+    image: >-
+      https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/04_yftun9.webp
+    gallery:
+      - >-
+        https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/04_yftun9.webp
+      - >-
+        https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/03_rtj8ky.webp
+      - >-
+        https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/01_bhqag1.webp
+      - >-
+        https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/02_ni6vws.webp
+  - name: Autodílna
+    shortDescription: Rozsáhlý prostor pro větší akce
+    longDescription: >
+      Náš největší prostor, který pojme až 50 účastníků. K dispozici je stylový
+      bar a komplet audioviuální vybavení.
+    image: >-
+      https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/09_gzauxj.webp
+    gallery:
       - >-
         https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/09_gzauxj.webp
-    gallery:
       - >-
         https://res.cloudinary.com/dkhdp7qmd/image/upload/v1729865364/08_ebvm4p.webp
       - >-
--- src/app/pronajem/page.tsx
+++ src/app/pronajem/page.tsx
@@ -1,12 +1,14 @@
 import racePrepImage from "@/images/autodilna/priprava-na-zavody.webp";
 import Testimonials from "@/app/ui/Testimonials";
 import { Metadata } from "next";
+import Image from "next/image";
 import { HeroSection } from "@/app/ui/HeroSection";
 import { PageLayout } from "@/app/ui/layout/PageLayout";
 import { UpcomingEventsCard } from "@/app/ui/UpcomingEventCard";
-import serviceCardImage from "@/images/homepage/serviceCard.webp";
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 import client from "../../../tina/__generated__/client";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
+import { ContactForm } from "@/app/ui/ContactForm";
 
 export const metadata: Metadata = {
 	title: "Racing 21 - O nás",
@@ -61,41 +63,105 @@ export default async function Page() {
 												<UpcomingEventsCard
 													title={prostor.name}
 													altImageText={"Obrázek konferenční místnosti"}
-													image={prostor?.image[0]}
+													image={prostor?.image}
 													description={prostor.shortDescription}
 												/>
 											</Tab>
 										),
 								)}
-								<Tab>
-									<UpcomingEventsCard
-										title={"Showroom"}
-										altImageText={"Servis vozidel"}
-										image={serviceCardImage}
-										description={"Lorem ipsum et donor"}
-									/>
-								</Tab>
-								<Tab>
-									<UpcomingEventsCard
-										title={"Autodílna"}
-										altImageText={"Servis vozidel"}
-										image={serviceCardImage}
-										description={"Lorem ipsum et donor"}
-									/>
-								</Tab>
 							</div>
 						</TabList>
 						<TabPanels className="mt-6">
-							<TabPanel>
-								<div
-									className={"bg-grayPrimary py-10 sm:py-120 rounded-lg px-6"}
-								></div>
-							</TabPanel>
-							<TabPanel>Content 2</TabPanel>
-							<TabPanel>Content 3</TabPanel>
+							{data.pronajem?.prostory?.map(
+								(prostor) =>
+									prostor && (
+										<TabPanel key={`tabPanel_${prostor.name}`}>
+											<div
+												className={
+													"bg-grayPrimary py-10 sm:py-120 rounded-lg px-6 flex gap-10 h-full"
+												}
+											>
+												<TabGroup className="flex flex-col-reverse w-1/2">
+													<div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
+														<TabList className="grid grid-cols-4 gap-6">
+															{prostor.gallery?.map((image) => (
+																<Tab
+																	key={image}
+																	className="group relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
+																>
+																	<span className="sr-only">
+																		{image}
+																	</span>
+																	<span className="absolute inset-0 overflow-hidden rounded-md">
+																		{image && (
+																			<Image
+																				alt=""
+																				priority
+																				src={image}
+																				width={200}
+																				height={150}
+																				className="h-full w-full object-cover object-center"
+																			/>
+																		)}
+																	</span>
+																	<span
+																		aria-hidden="true"
+																		className="pointer-events-none absolute inset-0 rounded-md ring-2 ring-transparent ring-offset-2 group-data-[selected]:ring-indigo-500"
+																	/>
+																</Tab>
+															))}
+														</TabList>
+													</div>
+
+													<TabPanels className="aspect-h-1 aspect-w-1 w-full">
+														{prostor.gallery?.map((image) => (
+															<TabPanel
+																key={image}
+																className={
+																	"relative h-[400px] w-full"
+																}
+															>
+																{image && (
+																	<Image
+																		alt={
+																			prostor.shortDescription
+																		}
+																		src={image}
+																		fill
+																		priority
+																		className="h-full w-full object-cover object-center sm:rounded-lg"
+																	/>
+																)}
+															</TabPanel>
+														))}
+													</TabPanels>
+												</TabGroup>
+												<div className={"h-[400px] flex flex-col"}>
+													<h2 className="font-bold text-2xl mb-2">
+														{prostor.name}
+													</h2>
+													<TinaMarkdown
+														content={prostor.longDescription}
+													/>
+													<button
+														type="button"
+														className="mt-auto w-[200px] rounded-full bg-red-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+													>
+														Zjistit dostupnost
+													</button>
+												</div>
+											</div>
+										</TabPanel>
+									),
+							)}
 						</TabPanels>
 					</TabGroup>
 				</div>
+				<div className="w-full flex flex-col">
+					<h2 className={"text-2xl capitalize font-bold mb-2"}>Kontaktní formulář</h2>
+					<ContactForm />
+				</div>
+
 				<div className="w-full flex flex-col">
 					<h2 className={"text-2xl font-bold mb-2"}>Řekli o nás</h2>
 					<Testimonials />
