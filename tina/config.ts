a1477047fb940793762ca6fab05807ebfe65729d
Jan Dudek <honza@honzadudek.cz>
2024-10-28 12:06:09 +0100

    feat(News): add Aktuality administration

--- tina/config.ts
+++ tina/config.ts
@@ -572,6 +572,68 @@ export default defineConfig({
 					},
 				],
 			},
+			{
+				name: "aktuality",
+				label: "Aktuality",
+				path: "aktuality",
+				fields: [
+					{
+						name: "aktuality",
+						label: "Aktuality",
+						type: "object",
+						list: true,
+						ui: {
+							itemProps: (item) => {
+								// Field values are accessed by item?.<Field name>
+								return { label: item?.name };
+							},
+						},
+						fields: [
+							{
+								type: "string",
+								name: "name",
+								label: "Titulek",
+								isTitle: true,
+								required: true,
+							},
+							{
+								type: "string",
+								name: "slug",
+								label: "URL název",
+								required: true,
+							},
+							{
+								type: "datetime",
+								name: "date",
+								label: "Datum",
+							},
+							{
+								type: "string",
+								name: "shortDescription",
+								label: "Krátký popis",
+								required: true,
+							},
+							{
+								type: "rich-text",
+								name: "longDescription",
+								label: "Text zprávy",
+								required: true,
+								isBody: true,
+							},
+							{
+								type: "image",
+								name: "image",
+								label: "Úvodní obrázek",
+							},
+							{
+								type: "image",
+								name: "gallery",
+								label: "Fotogalerie",
+							},
+						],
+					},
+				],
+			},
 		],
 	},
 });
