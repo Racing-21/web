e2e0213fe60d0aab809600835e2d1f4197451e78
Jan Dudek <honza@honzadudek.cz>
2024-10-28 12:23:24 +0100

    feat(News): update tina CMS

--- tina/config.ts
+++ tina/config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
 			return pack.TinaCloudCloudinaryMediaStore;
 		},
 	},
-	// See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+	// See docs on content modeling for more info on how to set up new content models: https://tina.io/docs/schema/
 	schema: {
 		collections: [
 			TinaUserCollection,
