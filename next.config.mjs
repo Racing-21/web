667b5d9ae166d1e04704ff826c66b708510a57dd
Jan Dudek <honza@honzadudek.cz>
2025-04-15 15:55:44 +0200

    chore(RC): update csp policy

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,30 @@ const nextConfig = {
 		],
 		domains: ["assets.tina.io", "res.cloudinary.com"],
 	},
+
+	// Add headers configuration
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: [
+							"default-src 'self'",
+							"script-src 'self' 'unsafe-inline' 'unsafe-eval' https://media-library.cloudinary.com",
+							"style-src 'self' 'unsafe-inline'",
+							"img-src 'self' data: https://storage.byznys21.net http://www.racing21.cz https://assets.tina.io https://res.cloudinary.com",
+							"font-src 'self' https://fonts.gstatic.com",
+							"connect-src 'self' https://fonts.googleapis.com",
+							"frame-src 'self'",
+							"object-src 'none'",
+						].join("; "),
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
